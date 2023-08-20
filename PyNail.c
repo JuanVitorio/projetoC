1ee6a0321074378b1eb8fa60095f03333f5da75f
Whoan10 <juanvitorioclash@gmail.com>
2023-08-20 18:27:49 -0300

    .

--- PyNail.c
+++ PyNail.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
+#include <locale.h>
 
 void informacoes(void);
+void infos_dev(void);
 
 int main(void)
 {
   informacoes();
+  infos_dev();
   return 0;
 }
 
 void informacoes(void)
 {
   printf("========================================================\n");
+  printf("||                    Informações gerais              ||\n");
+  printf("========================================================\n");
   printf("||                      UFRN | Caicó                  ||\n");
   printf("||           Disciplina DCT1106 -- Programação        ||\n");
   printf("||    Sistema de gestão para uma manicure e pedicure  ||\n");
   printf("||                   Dev: Juan Vitório                ||\n");
+  printf("||              Professor: Flavius Gorgônio           ||\n");
   printf("========================================================\n");
 }
 
+void infos_dev(void)
+{
+  printf("========================================================\n");
+  printf("||                     Desenvolvedor                  ||\n");
+  printf("========================================================\n");
+  printf("||                 Juan Vitório - BSI 2023            ||\n");
+  printf("||               juanvitorioclash@gmail.com           ||\n");
+  printf("||         Github: https://github.com/JuanVitorio     ||\n");
+  printf("========================================================\n");
+}
