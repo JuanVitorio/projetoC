b3434dbc10213bf60e4fcc17bdda4a4bccf13e39
Whoan10 <juanvitorioclash@gmail.com>
2023-08-20 15:55:38 -0300

    criei o arquivo e uma tela inicial básica

---
+++ PyNail.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void informacoes(void);
+
+int main(void)
+{
+  informacoes();
+  return 0;
+}
+
+void informacoes(void)
+{
+  printf("========================================================\n");
+  printf("||                      UFRN | Caicó                  ||\n");
+  printf("||           Disciplina DCT1106 -- Programação        ||\n");
+  printf("||    Sistema de gestão para uma manicure e pedicure  ||\n");
+  printf("||                   Dev: Juan Vitório                ||\n");
+  printf("========================================================\n");
+}
+
